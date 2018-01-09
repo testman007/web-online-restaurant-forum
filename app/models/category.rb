aea6ff77af5177507c4f147a63aefe5659373345
Kevin Ho <johnwater007@gmail.com>
2018-01-09 18:20:42 +0800

    add form on index page for creating new category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
-  #設定Modle關聯 => has_many 後面要接複數
+  # 確定新增餐廳分類時有輸入"分類名稱""
+  validates_presence_of :name
+  # 設定Modle關聯 => has_many 後面要接複數
   has_many :restaurants, dependent: :destroy
 end
