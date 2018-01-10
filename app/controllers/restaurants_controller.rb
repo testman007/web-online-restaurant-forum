a6ee73ab3ababd922fd0e9c3b4c21427b3b4bb8a
Kevin Ho <johnwater007@gmail.com>
2018-01-10 16:25:22 +0800

    update controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@ class RestaurantsController < ApplicationController
   # move (使用者登入的認證程序 before_action :authenticate_user!) to applicationController
   def index
     @restaurants = Restaurant.page(params[:page]).per(10)
+    @categories = Category.all
   end
   
 end
