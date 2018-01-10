f253b44dd1cdf3e9f1fe669496c32dd460a3c560
Kevin Ho <johnwater007@gmail.com>
2018-01-10 21:32:33 +0800

    setup route restaurant resources for public user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # 前台只需要action:index和show
+  # 此行程式同 resources :restaurants, except: [:new, :create, :edit, :update, :destroy]
+  resources :restaurants, only: [:index, :show]
+
   resources :categories, only: :show
   root "restaurants#index"
 
