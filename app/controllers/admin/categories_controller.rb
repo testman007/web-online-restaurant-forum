2af69d7e1be1fb534e55cdeeeda79cef460ab654
Kevin Ho <johnwater007@gmail.com>
2018-01-10 13:07:45 +0800

    user can update category with the same form on the index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,7 +4,12 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @category = Category.new
+
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
@@ -15,6 +20,17 @@ class Admin::CategoriesController < ApplicationController
     else
       @categories = Category.all
       render :index 
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:warning] = "category was successfully update"
+    else
+      @category = Category.all
+      render :index
     end
   end
   
