6e22f77592de0132c4921bd29629e3c8a9fe11fe
Kevin Ho <johnwater007@gmail.com>
2018-01-12 17:54:54 +0800

    為使用者建立 Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  # before_action :authenticate_user! => 在application_controller.rb 統一管理
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+    # before_action :set_user, only: [:show, :edit, :update]
+  end
+
+  def edit
+    # before_action :set_user, only: [:show, :edit, :update]
+  end
+
+  def update
+    # before_action :set_user, only: [:show, :edit, :update]
+    if @user.id == current_user.id
+      if @user.update(user_params)
+        flash[:info] = "User was successfully updated"
+        redirect_to user_path(@user)
+      else
+        flash.now[:info] = "User was failed to updated"
+        render :edit
+      end
+    else
+      flash.now[:info] = "You have no right to change this page"
+      render :edit
+    end
+  end
+
+  #
+  # private
+  #
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+
+  def set_user
+    #before_action
+    @user = User.find(params[:id])    
+  end
 end
