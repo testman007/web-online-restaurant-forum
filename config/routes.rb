bd7478eda78d2f48fd7b4c3df5fdbfe6a798e3e4
Kevin Ho <johnwater007@gmail.com>
2018-01-17 14:38:10 +0800

    setup routes for feeds and dashboard pages => 設定新路由feeds和dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,20 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show] do
     # 巢狀資源(nested resources)
     resources :comments, only: [:create, :destroy]
+    
+    # 瀏覽所有餐廳的最新動態
+    # 網址 Helper 使用複數的 restaurants
+    collection do
+      get :feeds  
+    end
+
+    # 瀏覽個別餐廳的 Dashboard
+    # 網址 Helper 使用單數的 restaurant
+    # 網址會帶入餐廳物件的 id，讓你可以在 Action 裡設定 
+    member do
+      get :dashboard
+    end
+
   end
   
   # setup category resources for public users
