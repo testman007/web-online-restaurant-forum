c7b5e2c6d68fb76b4acdd84587465d2e1abb44b1
Kevin Ho <johnwater007@gmail.com>
2018-01-23 14:04:30 +0800

    user can link to dashboard from restaurant show page => 使用者可以從餐廳的show頁面連結到餐廳的dashboard頁面

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,19 +1,35 @@
 class RestaurantsController < ApplicationController
   # move (使用者登入的認證程序 before_action :authenticate_user!) to applicationController
+  before_action :set_restaurant, only: [:show, :dashboard]
+
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
     @categories = Category.all
   end
   
   def show
-    @restaurant = Restaurant.find(params[:id])
+    # before_action :set_restaurant, only: [:show, :dashboard]
+
     # 評論的form_for需傳入@comment
     @comment = Comment.new
+  end
+
+  def dashboard
+    # before_action :set_restaurant, only: [:show, :dashboard]
   end
 
   # 展示最新的10筆餐廳和10筆評論
   def feeds
     @recent_restaurant = Restaurant.order(created_at: :desc).limit(10)
     @recent_comment = Comment.order(created_at: :desc).limit(10)
+  end
+
+  #
+  # private
+  #
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
