42743ed1d66ccf4ad6950b56eab857c8f33b365e
Kevin Ho <johnwater007@gmail.com>
2018-01-23 15:39:28 +0800

    add favorite / unfavorite action on RestaurantsController => 實作favorite的controller action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   # move (使用者登入的認證程序 before_action :authenticate_user!) to applicationController
-  before_action :set_restaurant, only: [:show, :dashboard]
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
 
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
@@ -8,20 +8,34 @@ class RestaurantsController < ApplicationController
   end
   
   def show
-    # before_action :set_restaurant, only: [:show, :dashboard]
+    # before_action :set_restaurant, only: [:show, :dashboard, :favorite]
 
     # 評論的form_for需傳入@comment
     @comment = Comment.new
   end
 
   def dashboard
-    # before_action :set_restaurant, only: [:show, :dashboard]
+    # before_action :set_restaurant, only: [:show, :dashboard, :favorite]
   end
 
   # 展示最新的10筆餐廳和10筆評論
   def feeds
     @recent_restaurant = Restaurant.order(created_at: :desc).limit(10)
     @recent_comment = Comment.order(created_at: :desc).limit(10)
+  end
+
+  # Favorite Controller Action
+  def favorite
+    # before_action :set_restaurant, only: [:show, :dashboard, :favorite]
+    @restaurant.favorites.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unfavorite
+    # before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
+    @favorites = Favorite.where(restaurant: @restaurant, user: current_user) 
+    @favorites.destroy_all # @favorites 可能多於一筆因此用destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   #
