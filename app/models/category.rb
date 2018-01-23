04e6600138ef8c3b9ed94d20cc9e55112139c6d5
Kevin Ho <johnwater007@gmail.com>
2018-01-23 14:23:54 +0800

    test for model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,8 @@ class Category < ApplicationRecord
   validates_presence_of :name
   # 設定Modle關聯 => has_many 後面要接複數
   has_many :restaurants, dependent: :destroy
+  # has_many :restaurants, dependent: :restrict_with_exception
+  # has_many :restaurants, dependent: :restrict_with_error
   # 確定餐廳分類的名稱不重複
   validates_uniqueness_of :name
 end
