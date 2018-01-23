599260d2f2c27a40ea3a717c37c454fa0adb661e
Kevin Ho <johnwater007@gmail.com>
2018-01-23 16:42:59 +0800

    add like controller action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   # move (使用者登入的認證程序 before_action :authenticate_user!) to applicationController
-  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
 
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
@@ -40,6 +40,25 @@ class RestaurantsController < ApplicationController
     # before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
     @favorites = Favorite.where(restaurant: @restaurant, user: current_user) 
     @favorites.destroy_all # @favorites 可能多於一筆因此用destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
+    # Favorite Controller Action
+  def like
+    # before_action :set_restaurant, only: [:show, :dashboard, :favorite]
+    if @restaurant.likes.exists?(:user_id => current_user.id)
+      # 若 table favorites已存在關聯就不再增加
+      flash.now[:info] = "You have alreday liked this restaurant!"
+    else 
+      @restaurant.likes.create!(user: current_user)
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def unlike
+    # before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
+    @likes = Like.where(restaurant: @restaurant, user: current_user) 
+    @likes.destroy_all # @favorites 可能多於一筆因此用destroy_all
     redirect_back(fallback_location: root_path)
   end
 
