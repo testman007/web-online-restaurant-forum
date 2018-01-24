000008fb341ee9cdcf03ff61e8873bfb2881c98d
Kevin Ho <johnwater007@gmail.com>
2018-01-24 16:40:18 +0800

    新增 Model 驗證，確保特定 user_id 下，只能有一個 followings_id

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,6 @@
 class Followship < ApplicationRecord
+  validates :following_id, uniqueness: { scope: :user_id }
+
   belongs_to :user, class_name: "User", foreign_key: "user_id", primary_key: "id"
   belongs_to :following, class_name: "User", foreign_key: "following_id", primary_key: "id"
 end
